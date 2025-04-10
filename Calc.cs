440e65d2f376b59169b56759625b43dfde30cc31
GitGit25 <contact@blue-phenix.com>
2025-04-10 09:53:13 +0200

    Ajoutez des fichiers projet.

---
+++ Calc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatriceMethodo
+{
+    public class Calc
+    {
+        public int Additionner(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using CalculatriceMethodo;
+    // See https://aka.ms/new-console-template for more information
+
+Calc calc = new Calc();
+int result =  calc.Additionner(2, 3);
+Console.WriteLine("mon resultat" + result);
